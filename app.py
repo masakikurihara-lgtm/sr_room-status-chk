0aab59cea9f49feb2e7e432d4b9255c4d43c4200
masakikurihara-lgtm <masaki.kurihara@gmail.com>
2025-12-15 11:18:57 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -887,7 +887,7 @@ def display_room_status(profile_data, input_room_id):
     st.markdown(html2, unsafe_allow_html=True)
 
     st.caption(
-        f"※取得できないデータなどはハイフン表示となる場合があります。"
+        f"※取得できないデータなどはハイフン表示となる場合があります。\n"
         f"※ライバルルームなどで、より詳細な情報や分析データ、見解等が欲しい場合はご相談ください。"
     )
 
